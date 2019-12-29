7d2a0e46eef710264ab9b94b270f05dc8e2a5132
Steven Frasica <sfrasica1@gmail.com>
2019-12-29 21:34:29 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,4 +44,3 @@ end# src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-
